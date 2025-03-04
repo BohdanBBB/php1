05781ab0ed2d3662a5033441f24ade0fb968b0ff
BohdanBBB <fasteronyt@gmail.com>
2025-03-04 10:45:54 +0200

    f

--- index.php
+++ index.php
@@ -7,8 +7,26 @@
 </head>
 <body>
     <?php
-    
-    
+        $meno = "Peter";
+        echo $meno;
+        echo '.<br>';
+        var_dump($meno);
+        $farby = array("cervena", "modra", "zelena");
+        echo $farby[0];
+        echo("<br>");
+
+        foreach($farby as $farba){
+            echo "Farba: $farba <br>";
+        }
+
+        $data = array("Peter" => 25,
+        "Jana" => 30,
+        "Marek" => 20
+            
+    );
+        foreach($data as $name => $age){
+            echo "$name ma $age rokov <br>";
+        }
     
     ?>
 </body>
